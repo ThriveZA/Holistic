87c0a6404b2024120665be3211cfb06fb0e3d125
Matthew Grant <matthew.grant95@hotmail.com>
2018-10-06 13:20:54 +0200

    Added LoginScreenController and LoginScreenViewnModel (empty)

---
+++ HolisticMain/HolisticMain/HolisticMain/ViewModels/LoginScreenViewModel/LoginScreenViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HolisticMain.ViewModels.LoginScreenViewModel
+{
+    public class LoginScreenViewModel
+    {
+    }
+}
